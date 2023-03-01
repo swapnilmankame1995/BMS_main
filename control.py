20bcda5062bb2cc0dd43bb091cb832a86b424f88
Swapnil mankame <swapnilum95@gmail.com>
2023-03-01 22:19:12 +0530

    Update control.py

--- control.py
+++ control.py
@@ -1,26 +1,3 @@
-# cell_1_voltage =  Voltage of Cell number 1
-# cell_2_voltage =  Voltage of Cell number 2
-# pack_voltage = Voltage of the entire battery pack (cell 1 + cell 2)
-
-# Balance_1 = Switch on balancing for cell 1
-# Balance_2 = Switch on balancing for cell 2
-
-# charge = Switch on Charging
-# discharge = Switch on Discharging
-
-# Stop = Stop all operations
-
-# ambient_temperature = Room temperature value
-
-
-# cell_1_temperature =  temperature of Cell number 1
-# cell_2_temperature =  temperature of Cell number 2
-
-# current = Total current passing through the circuit
-
-
-
-
 def control_algorithm(data,ser):
 
     iteration = float(data[0])
@@ -52,6 +29,31 @@ def control_algorithm(data,ser):
         print("\nMonitoring...\n", end='\r')
 
 # -----------------------------------Add Your Code/Algorithm here ----------------
+
+# Instructions
+        ## cell_1_voltage =  Voltage of Cell number 1
+        ## cell_2_voltage =  Voltage of Cell number 2
+        ## pack_voltage = Voltage of the entire battery pack (cell 1 + cell 2)
+
+        ## Balance_1 = Switch on balancing for cell 1
+        ## Balance_2 = Switch on balancing for cell 2
+
+        ## charge = Switch on Charging
+        ## discharge = Switch on Discharging
+
+        ## Stop = Stop all operations
+
+        ## ambient_temperature = Room temperature value
+
+
+        ## cell_1_temperature =  temperature of Cell number 1
+        ## cell_2_temperature =  temperature of Cell number 2
+
+        ## current = Total current passing through the circuit
+
+
+
+
     if iteration == 10:
         charge()
     elif iteration == 15:
@@ -66,13 +68,13 @@ def control_algorithm(data,ser):
 # ----------------------------------- Your Code/Algorithm here ----------------
 
 
-    if cell_1_voltage <= :
-        charge()
-    elif iteration == 15:
-        Discharge()
-    elif iteration == 20:
-        Balance_1()
-    elif iteration == 25:
-        Balance_2()
-    elif iteration == 30:
-        stop()
+    # if pack_voltage <= 5.5 :
+    #     charge()
+    # elif cell_1_voltage =< :
+    #     Discharge()
+    # elif iteration == 20:
+    #     Balance_1()
+    # elif iteration == 25:
+    #     Balance_2()
+    # elif pack_voltage <= 6.4:
+    #     stop()
